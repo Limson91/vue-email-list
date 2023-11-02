b133d2fc119d46b4fea84a56760712d1b1e0613c
Limson91 <matteo.a.colautti@gmail.com>
2023-11-02 15:29:05 +0100

    added email cicle and dom element

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 console.log('test ok');
 
+const emailElement = document.getElementById('email-list');
+
 for(let i=0; i<10; i++) {
     axios
     .get('https://flynn.boolean.careers/exercises/api/random/mail')
     .then((response) => {
-        console.log(response.data.response);
+        // console.log(response.data.response);
+
+        addEmail = response.data.response;
+        emailElement.innerHTML = emailElement.innerHTML + `<li>${addEmail}</li` 
     });
 }
